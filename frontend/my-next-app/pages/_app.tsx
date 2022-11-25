4077311be76c4c81fb12f9801a038adf3505df58
Mamadou DICKO <mamadoud@theodo.fr>
2022-11-25 16:33:53 +0100

    chore: upgrade types

--- frontend/my-next-app/pages/_app.tsx
+++ frontend/my-next-app/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, prefer-arrow/prefer-arrow-functions
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
